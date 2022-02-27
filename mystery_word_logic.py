0a8a7462940e79ae727cd2e135e36a05b612d598
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2022-02-27 07:53:37 -0500

    mvp

--- mystery_word_logic.py
+++ mystery_word_logic.py
@@ -2,35 +2,57 @@ import random
 
 
 def play_game():
-    # print("Hello Teddy")
     with open("test-word.txt") as file:
         text_words = file.read()
-        global words
-        words = list(map(str, text_words.split()))
-        the_words = random.choice(words)
-        count = len(the_words)
-        easy = 1, 2, 3, 4
-        normal = 5, 6, 7
-        hard = 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-        intro = input(
-            "Would you like to play a 4 letter game? [y/n] ")
-        if intro == "y":
-            intro2 = input("Which level would you like to play? (1, 2, 3) ")
-            print("Your word is", len(the_words), "letters long")
-        if intro == "n":
-            print("Okay, have a nice day!")
-            exit()
-        all_guesses = []
+    words = list(map(str, text_words.split()))
+    the_word = random.choice(words)
+    count = 8
+
+    # normal =
+    # hard =
+    intro = input(
+        "Would you like to play a word game? [y/n] ")
+    if intro == "y":
+        intro2 = input("Which level would you like to play? (1, 2, 3) ")
+        # if intro2 == 1:
+        #     len(the_word).range(5)
+        # if intro2 == 2:
+        #     len(the_word) <=
+        # if intro2 == 3:
+        #     len(the_word) <= 4
+        print("Your word is", len(the_word), "letters long")
+    if intro == "n":
+        print("Okay, have a nice day!")
+        exit()
+    incorrect_guesses = []
+    display = ["_"] * len(the_word)
     while count > 0:
+        if "_" not in display:
+            print("Congrats, you guessed the word.")
+            quit()
         if intro2 == "1":
+            guess = input(
+                "Guess one letter at a time. Make your guess: ").casefold()
+            # print([letter if letter in guesses else "_" for letter in the_words])
+        i = 0
+        if len(guess) != 1:
+            print("Please only use 1 character.")
+        elif guess in the_word:
+            for letter in the_word:
+                if letter == guess:
+                    display[i] = letter
+                i += 1
+            print(display)
+        else:
+            incorrect_guesses.append(guess)
             count = count - 1
-            guesses = input(
-                "Guess one letter at a time. Make your guess: ")
-        print([letter if letter in guesses else "_" for letter in the_words])
-        print("You have", count, "guesses left.")
-        if count > len(the_words):
-            print("GAME OVER no guesses left :(")
-            break
+            print(incorrect_guesses)
+            print("You have", count, "guesses left.")
+    print("GAME OVER no guesses left :( the correct answer is {}.".format(the_word))
+
+
+def difficulty():
+    pass
 
 
 if __name__ == "__main__":
