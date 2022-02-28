b0b0a1dba221ccb3b5f6febcdb8e4499d1adb55c
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2022-02-28 10:48:08 -0500

    difficulties

--- mystery_word_logic.py
+++ mystery_word_logic.py
@@ -1,42 +1,50 @@
 import random
+from colorama import Fore, Back, Style
+from colorama import init
+init()
+
+the_word = ""
 
 
 def play_game():
-    with open("test-word.txt") as file:
-        text_words = file.read()
-    words = list(map(str, text_words.split()))
-    the_word = random.choice(words)
+    global the_word
+    with open("words.txt") as file:
+        text_words = file.readlines()
+    words = [word.upper().strip() for word in text_words]
     count = 8
-
-    # normal =
-    # hard =
+    diction = dict()
+    for w in words:
+        diction[w] = len(w)
     intro = input(
-        "Would you like to play a word game? [y/n] ")
+        "Would you like to play hangman? [y/n] ").casefold()
     if intro == "y":
         intro2 = input("Which level would you like to play? (1, 2, 3) ")
-        # if intro2 == 1:
-        #     len(the_word).range(5)
-        # if intro2 == 2:
-        #     len(the_word) <=
-        # if intro2 == 3:
-        #     len(the_word) <= 4
-        print("Your word is", len(the_word), "letters long")
+    if intro2 == "1":
+        the_word = random.choice(
+            list({k: v for (k, v) in diction.items() if v >= 3 and v <= 5}))
+    elif intro2 == "2":
+        the_word = random.choice(
+            list({k: v for (k, v) in diction.items() if v >= 6 and v <= 8}))
+    elif intro2 == "3":
+        the_word = random.choice(
+            list({k: v for (k, v) in diction.items() if v >= 9}))
+    print(f"Your word is", {len(the_word)}, "letters long")
     if intro == "n":
         print("Okay, have a nice day!")
         exit()
     incorrect_guesses = []
     display = ["_"] * len(the_word)
     while count > 0:
-        if "_" not in display:
-            print("Congrats, you guessed the word.")
-            quit()
-        if intro2 == "1":
-            guess = input(
-                "Guess one letter at a time. Make your guess: ").casefold()
-            # print([letter if letter in guesses else "_" for letter in the_words])
         i = 0
+        if "_" not in display:
+            print(Fore.GREEN + "Congrats, you guessed the word." + Fore.RESET)
+            quit()
+        guess = input(
+            "Guess one letter at a time. Make your guess: ").upper()
         if len(guess) != 1:
-            print("Please only use 1 character.")
+            print(Fore.RED + "Please only use 1 character." + Fore.RESET)
+        elif guess in incorrect_guesses:
+            print(Fore.RED + "Your letter was already guessed." + Fore.RESET)
         elif guess in the_word:
             for letter in the_word:
                 if letter == guess:
@@ -47,12 +55,11 @@ def play_game():
             incorrect_guesses.append(guess)
             count = count - 1
             print(incorrect_guesses)
-            print("You have", count, "guesses left.")
-    print("GAME OVER no guesses left :( the correct answer is {}.".format(the_word))
-
-
-def difficulty():
-    pass
+            print(Fore.YELLOW + "Incorrect you have",
+                  count, "guesses left." + Fore.RESET)
+    print(Fore.RED +
+          "GAME OVER no guesses left :( the correct answer is {}.".format(the_word))
+    return the_word
 
 
 if __name__ == "__main__":
